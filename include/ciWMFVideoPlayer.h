01a64ada0888f64c527b4b7cc69908a15cfab4f9
morphogencc <morphogencc@users.noreply.github.com>
2022-04-10 22:13:49 +0000

    Apply clang-format

--- include/ciWMFVideoPlayer.h
+++ include/ciWMFVideoPlayer.h
@@ -18,122 +18,121 @@
 #endif*/
 
 #include "ciWMFVideoPlayerUtils.h"
-#include "presenter/EVRPresenter.h"
-#include "cinder/gl/gl.h"
 #include "cinder/Signals.h"
+#include "cinder/gl/gl.h"
+#include "presenter/EVRPresenter.h"
 
 class ciWMFVideoPlayer;
 class CPlayer;
 
 enum VideoFill {
-	FILL,	// fill rectangle (distort texture to fill)
-	ASPECT_FIT,	// fit rectangle and keep aspect ratio with blank space
-	CROP_FIT	// fit rectangle, keep aspect ratio and crop overflow
+    FILL,        // fill rectangle (distort texture to fill)
+    ASPECT_FIT,  // fit rectangle and keep aspect ratio with blank space
+    CROP_FIT     // fit rectangle, keep aspect ratio and crop overflow
 };
-
 
 typedef cinder::signals::Signal<void()> PlayStartedSignal;
 typedef std::shared_ptr<class ciWMFVideoPlayer> ciWMFVideoPlayerRef;
 
-class ciWMFVideoPlayer
-{
-	private:
-		static int mInstanceCount;
-		HWND mHWNDPlayer;
+class ciWMFVideoPlayer {
+   private:
+    static int mInstanceCount;
+    HWND mHWNDPlayer;
 
-		int mWidth;
-		int mHeight;
+    int mWidth;
+    int mHeight;
 
-		bool mWaitForLoadedToPlay;
-		bool mIsLooping;
-		VideoFill mVideoFill;
-		bool mIsSharedTextureLocked = false;
+    bool mWaitForLoadedToPlay;
+    bool mIsLooping;
+    VideoFill mVideoFill;
+    bool mIsSharedTextureLocked = false;
 
-		bool mSharedTextureCreated;
+    bool mSharedTextureCreated;
 
-		ci::gl::TextureRef mTex;
+    ci::gl::TextureRef mTex;
 
-		cinder::signals::Connection mWinCloseConnection;
+    cinder::signals::Connection mWinCloseConnection;
 
-		BOOL InitInstance();
-		void OnPlayerEvent( HWND hwnd, WPARAM pUnkPtr );
+    BOOL InitInstance();
+    void OnPlayerEvent(HWND hwnd, WPARAM pUnkPtr);
 
-		PlayStartedSignal       mPlayStartedSignal;
-		bool                    mPlayPending;
-		bool                    mIsAudioOnly;
+    PlayStartedSignal mPlayStartedSignal;
+    bool mPlayPending;
+    bool mIsAudioOnly;
 
-	public:
-		friend struct ScopedVideoTextureBind;
-		struct ScopedVideoTextureBind : private ci::Noncopyable {
-			public:
-				ScopedVideoTextureBind( const ciWMFVideoPlayerRef video, uint8_t textureUnit );
-				ScopedVideoTextureBind( const ciWMFVideoPlayer& video, uint8_t textureUnit );
-				~ScopedVideoTextureBind();
+   public:
+    friend struct ScopedVideoTextureBind;
+    struct ScopedVideoTextureBind : private ci::Noncopyable {
+       public:
+        ScopedVideoTextureBind(const ciWMFVideoPlayerRef video, uint8_t textureUnit);
+        ScopedVideoTextureBind(const ciWMFVideoPlayer& video, uint8_t textureUnit);
+        ~ScopedVideoTextureBind();
 
-			private:
-				ci::gl::Context* mCtx;
-				GLenum mTarget;
-				uint8_t mTextureUnit;
-				CPlayer* mPlayer;
-		};
+       private:
+        ci::gl::Context* mCtx;
+        GLenum mTarget;
+        uint8_t mTextureUnit;
+        CPlayer* mPlayer;
+    };
 
-		CPlayer* mPlayer;
-		int mId;
+    CPlayer* mPlayer;
+    int mId;
 
-		ciWMFVideoPlayer();
-		~ciWMFVideoPlayer();
+    ciWMFVideoPlayer();
+    ~ciWMFVideoPlayer();
 
-		bool loadMovie( const ci::fs::path& filePath, const std::string& audioDevice = "", bool isAudioOnly = false );
-		void close();
-		void update();
+    bool loadMovie(const ci::fs::path& filePath, const std::string& audioDevice = "", bool isAudioOnly = false);
+    void close();
+    void update();
 
-		void play();
-		void stop();
-		void pause();
+    void play();
+    void stop();
+    void pause();
 
-		float getPosition();
-		float getDuration();
-		float getFrameRate();
-		float getVolume();
+    float getPosition();
+    float getDuration();
+    float getFrameRate();
+    float getVolume();
 
-		void setPosition( float pos );
-		void stepForward();
-		void setVolume( float vol );
+    void setPosition(float pos);
+    void stepForward();
+    void setVolume(float vol);
 
-		float getHeight();
-		float getWidth();
-		inline ci::vec2 getSize() { return ci::vec2(getWidth(), getHeight()); }
+    float getHeight();
+    float getWidth();
+    inline ci::vec2 getSize() { return ci::vec2(getWidth(), getHeight()); }
 
-		bool isPlaying();
-		bool isStopped();
-		bool isPaused();
+    bool isPlaying();
+    bool isStopped();
+    bool isPaused();
 
-		void bind(uint8_t texture_unit);
-		void unbind();
+    void bind(uint8_t texture_unit);
+    void unbind();
 
-		ci::ImageSourceRef getImageSource();
-		bool isTexturedLocked()const { return mIsSharedTextureLocked; }
+    ci::ImageSourceRef getImageSource();
+    bool isTexturedLocked() const { return mIsSharedTextureLocked; }
 
-		bool setSpeed( float speed, bool useThinning = false ); //thinning drops delta frames for faster playback though appears to be choppy, default is false
-		float getSpeed();
+    bool setSpeed(float speed, bool useThinning = false);  // thinning drops delta frames for faster playback though
+                                                           // appears to be choppy, default is false
+    float getSpeed();
 
-		void setLoop( bool isLooping );
-		bool isLooping() const { return mIsLooping; }
+    void setLoop(bool isLooping);
+    bool isLooping() const { return mIsLooping; }
 
-		bool hasTexture() const { return ( mPlayer && mTex ); }
-		ci::vec2 getTextureSize();
+    bool hasTexture() const { return (mPlayer && mTex); }
+    ci::vec2 getTextureSize();
 
-		void setVideoFill( VideoFill videoFill ) { mVideoFill = videoFill; }
+    void setVideoFill(VideoFill videoFill) { mVideoFill = videoFill; }
 
-		void draw( int x, int y, int w, int h );
-		void draw( int x, int y ) { draw( x, y, getWidth(), getHeight() ); }
+    void draw(int x, int y, int w, int h);
+    void draw(int x, int y) { draw(x, y, getWidth(), getHeight()); }
 
-		HWND getHandle() const { return mHWNDPlayer; }
-		LRESULT WndProc( HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam );
+    HWND getHandle() const { return mHWNDPlayer; }
+    LRESULT WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
 
-		PresentationEndedSignal& getPresentationEndedSignal();
+    PresentationEndedSignal& getPresentationEndedSignal();
 
-		PlayStartedSignal&      getPlayStartedSignal() { return mPlayStartedSignal; }
+    PlayStartedSignal& getPlayStartedSignal() { return mPlayStartedSignal; }
 
-		static void forceExit();
+    static void forceExit();
 };
--- samples/SimplePlayback/src/SimplePlaybackApp.cpp
+++ samples/SimplePlayback/src/SimplePlaybackApp.cpp
@@ -1,10 +1,9 @@
+#include "ciWMFVideoPlayer.h"
+#include "cinder/Log.h"
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 #include "cinder/params/Params.h"
-#include "cinder/app/RendererGl.h"
-#include "cinder/Log.h"
-#include "ciWMFVideoPlayer.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -15,60 +14,55 @@ using namespace ci::app;
 using namespace std;
 
 class SimplePlaybackApp : public App {
-	public:
-		SimplePlaybackApp();
-		~SimplePlaybackApp();
-		void setup();
-		void update();
-		void draw();
-		void mouseDown(ci::app::MouseEvent event) override;
-	private:
-		bool mVideoSetup;
-		ciWMFVideoPlayer mVideo1;
-		ci::params::InterfaceGlRef mParams;
-		float mFps;
+   public:
+    SimplePlaybackApp();
+    ~SimplePlaybackApp();
+    void setup();
+    void update();
+    void draw();
+    void mouseDown(ci::app::MouseEvent event) override;
+
+   private:
+    bool mVideoSetup;
+    ciWMFVideoPlayer mVideo1;
+    ci::params::InterfaceGlRef mParams;
+    float mFps;
 };
 
-SimplePlaybackApp::SimplePlaybackApp() :
-	mFps(0.0f)
-{
-}
+SimplePlaybackApp::SimplePlaybackApp() : mFps(0.0f) {}
 
-SimplePlaybackApp::~SimplePlaybackApp()
-{
-}
+SimplePlaybackApp::~SimplePlaybackApp() {}
 
 void SimplePlaybackApp::setup() {
-	std::string videoPath = getAssetPath("1.wmv").string();
-	mVideo1.loadMovie(videoPath, "Headphones (High Definition Audio Device)");
-	mVideo1.play();
-	mVideo1.setLoop(true);
-	mVideo1.getPresentationEndedSignal().connect([]() {
-		ci::app::console() << "Video finished playing!" << std::endl;
-		});
+    std::string videoPath = getAssetPath("1.wmv").string();
+    mVideo1.loadMovie(videoPath, "Headphones (High Definition Audio Device)");
+    mVideo1.play();
+    mVideo1.setLoop(true);
+    mVideo1.getPresentationEndedSignal().connect(
+        []() { ci::app::console() << "Video finished playing!" << std::endl; });
 
-	mParams = params::InterfaceGl::create("Params", ivec2(210, 210));
-	mParams->addParam<float>("FPS", &mFps, true);
+    mParams = params::InterfaceGl::create("Params", ivec2(210, 210));
+    mParams->addParam<float>("FPS", &mFps, true);
 }
 
-void SimplePlaybackApp::mouseDown( MouseEvent event ) {
-	CI_LOG_I("Video position: " << mVideo1.getPosition());
+void SimplePlaybackApp::mouseDown(MouseEvent event) {
+    CI_LOG_I("Video position: " << mVideo1.getPosition());
 }
 
 void SimplePlaybackApp::update() {
-	mFps = getAverageFps();
-	mVideo1.update();
+    mFps = getAverageFps();
+    mVideo1.update();
 }
 
 void SimplePlaybackApp::draw() {
-	gl::clear(Color(0, 0, 0));
-	mVideo1.draw(0, 0);
+    gl::clear(Color(0, 0, 0));
+    mVideo1.draw(0, 0);
 
-	mParams->draw();
+    mParams->draw();
 }
 
 CINDER_APP(SimplePlaybackApp, RendererGl, [](App::Settings* settings) {
-		settings->setFrameRate(60.0f);
-		settings->prepareWindow(Window::Format().size(1280, 720).title("Simple Playback"));
-		settings->setConsoleWindowEnabled(true);
+    settings->setFrameRate(60.0f);
+    settings->prepareWindow(Window::Format().size(1280, 720).title("Simple Playback"));
+    settings->setConsoleWindowEnabled(true);
 })
